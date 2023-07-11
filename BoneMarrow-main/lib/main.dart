a6c620c6892919beb9a01ee6a5382710e99ab8a2
ibrahim <ibrahimmansour58563@gmail.com>
2023-07-11 20:58:42 +0300

    launch-problem

--- BoneMarrow-main/lib/main.dart
+++ BoneMarrow-main/lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:g_project/core/repos/dio_helper.dart';
 import 'package:g_project/core/repos/shared_pref.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-//import 'homePage.dart';
-import 'main_bottom_navigation_bar.dart';
-import 'not_refactored/splash_Screen.dart';
+import 'screens/not_refactored/splash_Screen.dart';
 
 late SharedPreferences shared;
 
